8697a6af2fb02346a72181cf116b3156ef91d46f
Mitzie Magdadaro <mitzmagdadaro@gmail.com>
2019-04-02 11:18:37 +0800

    added functions on loginchecker
    if user is login and try to access unauthorized pages

--- auth/admin/logincheck.php
+++ auth/admin/logincheck.php
@@ -1,8 +1,45 @@
 <?php 
   if(!isset($_SESSION['username']) || empty($_SESSION['username']))
   {
+    
      echo "<script>alert('Please log in first.');</script>";
      header("Location: ../../index.php");
      exit();
+  }
+  else {  //log in check if same office user
+    redirect();
+  }
+  function redirect()
+  {
+    if($_SESSION['accntID'] == 1)
+    {
+
+    }
+    else
+    {
+      if($_SESSION['accntID'] == 2){
+        header ("Location: /pupwebdev/auth/schoolAdmin/index.php");
+        $con=null;
+  
+      }
+      else if($_SESSION['accntID'] == 3)
+      {
+      header("Location: /pupwebdev/auth/acadservice/acadService_Scheduler.php");
+      $con=null;
+      }
+      else if($_SESSION["accountType"] == 'prof')
+      {
+          header("Location: /pupwebdev/auth/office/queuePerOffice.php");
+          $con=null;
+      }
+      else
+      {
+        $message = 'catcher';
+      // echo "<script> alert('".$message."'); </script>"; 
+      header("Location: ../../index.php");
+        unset($_SESSION['username']);
+        session_destroy();
+      }
+    }
   }
 ?>
