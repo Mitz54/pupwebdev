c11a5af402005687e6b5fde25b72d2b70f99fb00
pioesmeria <46041054+pioesmeria@users.noreply.github.com>
2019-04-27 09:32:09 +0800

    Edit breakbutton and nextbutton

--- auth/schoolAdmin/queueTable.php
+++ auth/schoolAdmin/queueTable.php
@@ -3,13 +3,7 @@
 	include ($_SERVER['DOCUMENT_ROOT'].'/pupwebdev/auth/dbConnect.php');
 	$dateToday = date("Y-m-d");
 	$staffID = $_SESSION['accntID'];
-	$num = $_POST['queueNewCount'];
-	if($num == 1){
-				$profNewCount = $_SESSION['PendingNum'];
-	}
-	else {
-			$profNewCount = $_SESSION['PendingNum'];
-	}
+	$profNewCount = $_SESSION['PendingNum'];
 	//$sql2 = "UPDATE `queueingtransaction` INNER JOIN queue ON queue.queueNumber = queueingtransaction.queueID_FK SET `queueingTransactionStatus` = 'Done' WHERE `queue`.`queueNumber` = $profNewCount;";
 	$result2 = mysqli_query($con,"Call updateQueueTable('$profNewCount','$staffID','$dateToday')");
 	include ($_SERVER['DOCUMENT_ROOT'].'/pupwebdev/auth/dbConnect.php');
